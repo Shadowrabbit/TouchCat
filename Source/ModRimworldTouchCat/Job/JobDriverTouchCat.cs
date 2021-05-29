9d051bc2caa1cacc36ff96cccac3d56f5c626627
yingtu0401@gmail.com <yingtu0401@gmail.com>
2021-05-29 23:36:11 +0800

    调整野人撸猫相关事件不会提示

--- Source/ModRimworldTouchCat/Job/JobDriverTouchCat.cs
+++ Source/ModRimworldTouchCat/Job/JobDriverTouchCat.cs
@@ -15,78 +15,89 @@ using Verse;
 
 namespace SR.ModRimWorldTouchCat
 {
-	[UsedImplicitly]
-	public class JobDriverTouchCat : JobDriverTouchPet
-	{
-		/// <summary>
-		/// 全部步骤成功时回调
-		/// </summary>
-		protected override void OnToilsSuccess()
-		{
-			//触发撸猫的回忆
-			pawn.needs.mood.thoughts.memories.TryGainMemory(ThoughtDefOf.SrThoughtTouchCat);
-			//概率触发成瘾	
-			CalcAddiction();
-			//概率加入殖民者阵营
-			CalcJoin();
-			//计算需求
-			CalcNeedTouchCat();
-		}
+    [UsedImplicitly]
+    public class JobDriverTouchCat : JobDriverTouchPet
+    {
+        /// <summary>
+        /// 全部步骤成功时回调
+        /// </summary>
+        protected override void OnToilsSuccess()
+        {
+            //触发撸猫的回忆
+            pawn.needs.mood.thoughts.memories.TryGainMemory(ThoughtDefOf.SrThoughtTouchCat);
+            //概率触发成瘾	
+            CalcAddiction();
+            //概率加入殖民者阵营
+            CalcJoin();
+            //计算需求
+            CalcNeedTouchCat();
+        }
 
-		/// <summary>
-		/// 计算成瘾触发
-		/// </summary>
-		private void CalcAddiction()
-		{
-			//已经成瘾 不会重复触发
-			if (Enumerable.Any(pawn.health.hediffSet.hediffs, heddif => heddif.def == HediffDefOf.SrHediffAddictionTouchCat))
-			{
-				return;
-			}
-			var randomNum = Random.Range(0f, 1f);
-			if ((randomNum > ChanceToAddiction))
-			{
-				return;
-			}
-			var hediffAddictionTouchCat = HediffMaker.MakeHediff(HediffDefOf.SrHediffAddictionTouchCat, pawn);
-			pawn.health.AddHediff(hediffAddictionTouchCat);
-			Messages.Message("MsgAddictionTouchCat".Translate(pawn.Label), MessageTypeDefOf.NeutralEvent);
-		}
+        /// <summary>
+        /// 计算成瘾触发
+        /// </summary>
+        private void CalcAddiction()
+        {
+            //已经成瘾 不会重复触发
+            if (Enumerable.Any(pawn.health.hediffSet.hediffs,
+                heddif => heddif.def == HediffDefOf.SrHediffAddictionTouchCat))
+            {
+                return;
+            }
 
-		/// <summary>
-		/// 计算加入殖民者
-		/// </summary>
-		private void CalcJoin()
-		{
-			//已经是殖民者阵营
-			if (Pet.Faction == Faction.OfPlayer)
-			{
-				return;
-			}
-			var randomNum = Random.Range(0f, 1f);
-			if ((randomNum > ChanceToJoin))
-			{
-				return;
-			}
-			Pet.SetFaction(Faction.OfPlayer);
-			Messages.Message("MsgTouchPetJoin".Translate(pawn.Label, Pet.Label), MessageTypeDefOf.NeutralEvent);
-		}
+            var randomNum = Random.Range(0f, 1f);
+            if ((randomNum > ChanceToAddiction))
+            {
+                return;
+            }
 
-		/// <summary>
-		/// 计算撸猫需求
-		/// </summary>
-		private void CalcNeedTouchCat()
-		{
-			//如果存在撸猫需求 恢复到最高水平
-			var allNeeds = pawn.needs.AllNeeds;
-			var needTouchCat = allNeeds.Where(t => t.def == NeedDefOf.SrNeedTouchCat).Cast<NeedTouchPet>()
-				.FirstOrDefault();
-			//当前角色不存在撸猫需求
-			if (needTouchCat == null)
-			{
-				return;
-			}
-			needTouchCat.CurLevel = 1f;
-		}
-	}
+            var hediffAddictionTouchCat = HediffMaker.MakeHediff(HediffDefOf.SrHediffAddictionTouchCat, pawn);
+            pawn.health.AddHediff(hediffAddictionTouchCat);
+            //玩家阵营的小人上瘾会提示
+            if (pawn.Faction == Faction.OfPlayer)
+            {
+                Messages.Message("MsgAddictionTouchCat".Translate(pawn.Label), MessageTypeDefOf.NeutralEvent);
+            }
+        }
+
+        /// <summary>
+        /// 计算加入殖民者
+        /// </summary>
+        private void CalcJoin()
+        {
+            //阵营相同
+            if (Pet.Faction == pawn.Faction)
+            {
+                return;
+            }
+
+            var randomNum = Random.Range(0f, 1f);
+            if ((randomNum > ChanceToJoin))
+            {
+                return;
+            }
+
+            Pet.SetFaction(pawn.Faction);
+            Messages.Message("MsgTouchPetJoin".Translate(pawn.Label, Pet.Label, pawn.Faction),
+                MessageTypeDefOf.NeutralEvent);
+        }
+
+        /// <summary>
+        /// 计算撸猫需求
+        /// </summary>
+        private void CalcNeedTouchCat()
+        {
+            //如果存在撸猫需求 恢复到最高水平
+            var allNeeds = pawn.needs.AllNeeds;
+            var needTouchCat = allNeeds.Where(t => t.def == NeedDefOf.SrNeedTouchCat).Cast<NeedTouchPet>()
+                .FirstOrDefault();
+            //当前角色不存在撸猫需求
+            if (needTouchCat == null)
+            {
+                return;
+            }
+
+            needTouchCat.CurLevel = 1f;
+        }
+    }
 }
--- Source/ModRimworldTouchCat/Job/JobDriverTouchRabbit.cs
+++ Source/ModRimworldTouchCat/Job/JobDriverTouchRabbit.cs
@@ -15,79 +15,89 @@ using Verse;
 
 namespace SR.ModRimWorldTouchCat
 {
-	[UsedImplicitly]
-	public class JobDriverTouchRabbit : JobDriverTouchPet
-	{
-		/// <summary>
-		/// 全部步骤成功时回调
-		/// </summary>
-		protected override void OnToilsSuccess()
-		{
-			//触发撸兔子的回忆
-			pawn.needs.mood.thoughts.memories.TryGainMemory(ThoughtDefOf.SrThoughtTouchRabbit);
-			//概率触发成瘾	
-			CalcAddiction();
-			//概率加入殖民者阵营
-			CalcJoin();
-			//计算需求值
-			CalcNeedTouchRabbit();
-		}
+    [UsedImplicitly]
+    public class JobDriverTouchRabbit : JobDriverTouchPet
+    {
+        /// <summary>
+        /// 全部步骤成功时回调
+        /// </summary>
+        protected override void OnToilsSuccess()
+        {
+            //触发撸兔子的回忆
+            pawn.needs.mood.thoughts.memories.TryGainMemory(ThoughtDefOf.SrThoughtTouchRabbit);
+            //概率触发成瘾	
+            CalcAddiction();
+            //概率加入殖民者阵营
+            CalcJoin();
+            //计算需求值
+            CalcNeedTouchRabbit();
+        }
 
-		/// <summary>
-		/// 计算成瘾触发
-		/// </summary>
-		private void CalcAddiction()
-		{
-			//已经成瘾 不会重复触发
-			if (Enumerable.Any(pawn.health.hediffSet.hediffs, heddif => heddif.def == HediffDefOf.SrHediffAddictionTouchRabbit))
-			{
-				return;
-			}
-			var randomNum = Random.Range(0f, 1f);
-			if ((randomNum > ChanceToAddiction))
-			{
-				return;
-			}
-			var heddifAddictionTouchRabbit = HediffMaker.MakeHediff(HediffDefOf.SrHediffAddictionTouchRabbit, pawn);
-			pawn.health.AddHediff(heddifAddictionTouchRabbit);
-			Messages.Message("MsgAddictionTouchRabbit".Translate(pawn.Label), MessageTypeDefOf.NeutralEvent);
-		}
+        /// <summary>
+        /// 计算成瘾触发
+        /// </summary>
+        private void CalcAddiction()
+        {
+            //已经成瘾 不会重复触发
+            if (Enumerable.Any(pawn.health.hediffSet.hediffs,
+                heddif => heddif.def == HediffDefOf.SrHediffAddictionTouchRabbit))
+            {
+                return;
+            }
 
-		/// <summary>
-		/// 计算加入殖民者
-		/// </summary>
-		private void CalcJoin()
-		{
-			//已经是殖民者阵营
-			if (Pet.Faction == Faction.OfPlayer)
-			{
-				return;
-			}
-			var randomNum = Random.Range(0f, 1f);
-			if ((randomNum > ChanceToJoin))
-			{
-				return;
-			}
-			Pet.SetFaction(Faction.OfPlayer);
-			Messages.Message("MsgTouchPetJoin".Translate(pawn.Label, Pet.Label), MessageTypeDefOf.NeutralEvent);
-		}
+            var randomNum = Random.Range(0f, 1f);
+            if ((randomNum > ChanceToAddiction))
+            {
+                return;
+            }
 
-		/// <summary>
-		/// 计算撸兔子需求
-		/// </summary>
-		private void CalcNeedTouchRabbit()
-		{
-			//如果存在撸猫需求 恢复到最高水平
-			var allNeeds = pawn.needs.AllNeeds;
-			var needTouchRabbit = allNeeds.Where(t => t.def == NeedDefOf.SrNeedTouchRabbit).Cast<NeedTouchPet>()
-				.FirstOrDefault();
-			//当前角色不存在撸猫需求
-			if (needTouchRabbit == null)
-			{
-				return;
-			}
+            var heddifAddictionTouchRabbit = HediffMaker.MakeHediff(HediffDefOf.SrHediffAddictionTouchRabbit, pawn);
+            pawn.health.AddHediff(heddifAddictionTouchRabbit);
+            //玩家阵营的小人上瘾会提示
+            if (pawn.Faction == Faction.OfPlayer)
+            {
+                Messages.Message("MsgAddictionTouchRabbit".Translate(pawn.Label), MessageTypeDefOf.NeutralEvent);
+            }
+        }
 
-			needTouchRabbit.CurLevel = 1f;
-		}
-	}
+        /// <summary>
+        /// 计算加入殖民者
+        /// </summary>
+        private void CalcJoin()
+        {
+            //阵营相同
+            if (Pet.Faction == pawn.Faction)
+            {
+                return;
+            }
+
+            var randomNum = Random.Range(0f, 1f);
+            if ((randomNum > ChanceToJoin))
+            {
+                return;
+            }
+
+            Pet.SetFaction(pawn.Faction);
+            Messages.Message("MsgTouchPetJoin".Translate(pawn.Label, Pet.Label, pawn.Faction),
+                MessageTypeDefOf.NeutralEvent);
+        }
+
+        /// <summary>
+        /// 计算撸兔子需求
+        /// </summary>
+        private void CalcNeedTouchRabbit()
+        {
+            //如果存在撸猫需求 恢复到最高水平
+            var allNeeds = pawn.needs.AllNeeds;
+            var needTouchRabbit = allNeeds.Where(t => t.def == NeedDefOf.SrNeedTouchRabbit).Cast<NeedTouchPet>()
+                .FirstOrDefault();
+            //当前角色不存在撸猫需求
+            if (needTouchRabbit == null)
+            {
+                return;
+            }
+
+            needTouchRabbit.CurLevel = 1f;
+        }
+    }
 }
